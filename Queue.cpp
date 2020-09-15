e8d835dcca7b0c2086011b08829a67a4ae8fb618
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2020-09-15 21:56:37 +0530

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -1,3 +1,105 @@
+/////////////////////
+/*Using Linked List*/
+/////////////////////
+#include<bits/stdc++.h>
+using namespace std;
+
+class QueueList {
+private:
+    struct Node {
+        int data;
+        Node* next;
+    };
+    Node* front;
+    Node* rear;
+public:
+    QueueList() {
+        front = rear = NULL;
+    }
+    void enqueue(int x);
+    void dequeue();
+    void printQueue();
+};
+
+void QueueList::enqueue(int x) {
+    Node* n = new Node;
+    n->data = x;
+    n->next = NULL;
+    if(rear==NULL) {
+        front = n;
+        rear = n;
+        return;
+    }
+    rear->next = n;
+    rear = n;
+}
+
+void QueueList::dequeue() {
+    if(front==NULL) {
+        // printf("Empty Queue\n");
+        return;
+    }
+    if(front==rear) {
+        front = NULL;
+        rear = NULL;
+        return;
+    }
+    Node* temp = front;
+    front = front->next;
+    free(temp);
+}
+
+void QueueList::printQueue() {
+    if(front==NULL) {
+        printf("Empty Queue\n");
+        return;
+    }
+    Node* temp = front;
+    while(temp!=NULL) {
+        printf("%d ", temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    QueueList qL;
+    cout << "\t\t# QueueList #\n\n";
+    
+    cout << "1. enqueue()\n";
+    cout << "2. dequeue()\n";
+    cout << "3. printQueue()\n";
+    cout << "4. Exit\n";
+    
+    int choice = 0;
+    while(choice != 4) {
+        cout << "\nChoice: ";
+        cin>>choice;
+        switch(choice) {
+            case 1: {
+                int x;
+                cout << "Enter the element: ";
+                cin>>x;
+                qL.enqueue(x); break;
+            }
+            case 2: qL.dequeue(); break;
+            case 3: qL.printQueue(); break;
+            default: continue;
+        }
+    }
+    
+    cout << "\n\nDo you want to do it again(y/n): ";
+	char ch;
+	cin >> ch;
+	if(ch=='y'||ch=='Y') main();
+	
+    return 0;
+}
+
+
+///////////////
+/*Using Array*/
+///////////////
 #include <bits/stdc++.h>
 using namespace std;
 
