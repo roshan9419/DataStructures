0d9f1a280333d3233a0c8a40c3c5ce0de0e1e2a4
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2020-09-28 22:40:35 +0530

    Update QuickSort.cpp

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -7,7 +7,7 @@ QuickSort(A, p, r) {
 	}
 }
 PARTITION(A, p, r) {
-	x = A[r]
+	x = A[r] //pivot
 	i = p-1
 	for j=p to r-1 {
 		if A[j]<=x {
@@ -26,6 +26,45 @@ WC - O(n^2) //if array is already sorted
 
 PIVOT NODE::
 the element through which we are sorting, like we can take the last or first node, and elements to its left are smaller and elements after it must be greater
-
-
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int a[], int n);
+void QuickSort(int a[], int p, int r);
+int PARTITION(int a[], int p, int r);
+
+void QuickSort(int a[], int p, int r) {
+	if(p<r) {
+		int q = PARTITION(a, p, r);
+		QuickSort(a, p, q-1);
+		QuickSort(a, q+1, r);
+	}
+}
+
+int PARTITION(int a[], int p, int r) {
+	int pivot = a[r];
+	int i=p-1;
+	for(int j=p; j<=r-1; j++) {
+		if(a[j]<=pivot) {
+			i++;
+			swap(a[i], a[j]);
+		}
+	}
+	swap(a[i+1], a[r]);
+	return i+1;
+}
+
+int main() {
+	int a[10] = {2, 10, 9, 3, 5, 8, 6, 7, 1, 4};
+	print(a, 10);
+	QuickSort(a, 0, 9);
+	print(a, 10);
+	return 0;
+}
+
+void print(int a[], int n) {
+	for(int i=0; i<n; i++) cout << a[i] << " ";
+	cout << endl;
+}
